469da57e8739c6c58d557c16fe78ff59d16614ba
Keyang Xiang <kxiang@Keyangs-MacBook-Pro.local>
2013-06-26 16:29:30 +0100

    new pdf

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,7 @@
 var tg=require("thumbnailgen");
 
 module.exports.gen=function (params,cb){
-    tg.pdf.generateFromFilePath(__dirname+"/WV Presentation Q1 2013.pdf",__dirname+"/public/WV Presentation Q1 2013.png",function(err,fp){
+    tg.pdf.generateFromFilePath(__dirname+"/Press 2012.09.11 Telegraph.co.uk DofC.pdf",__dirname+"/public/Press 2012.09.11 Telegraph.co.uk DofC.png",function(err,fp){
         console.log(err);
         console.log(fp);
         cb(err,{"path":fp});
