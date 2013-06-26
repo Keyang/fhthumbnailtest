99ff91adad9c3df0e05efeab910c594e47d33c2e
Keyang Xiang <kxiang@Keyangs-MacBook-Pro.local>
2013-06-26 11:44:20 +0100

    added file path

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,7 @@
 var tg=require("thumbnailgen");
 
 module.exports.gen=function (params,cb){
-    tg.pdf.generateFromFilePath(__dirname+"/test.pdf",function(err,fp){
+    tg.pdf.generateFromFilePath(__dirname+"/test.pdf",__dirname+"/public/test.pdf",function(err,fp){
         console.log(err);
         console.log(fp);
         cb(err,{"path":fp});
