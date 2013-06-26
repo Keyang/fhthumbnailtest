4419b9b1305fa64f0a05d86f178846e94fd0832d
Keyang Xiang <kxiang@Keyangs-MacBook-Pro.local>
2013-06-26 12:37:14 +0100

    use new test file

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,7 @@
 var tg=require("thumbnailgen");
 
 module.exports.gen=function (params,cb){
-    tg.pdf.generateFromFilePath(__dirname+"/test.pdf",__dirname+"/public/test.pdf",function(err,fp){
+    tg.pdf.generateFromFilePath(__dirname+"/WV Presentation Q1 2013.pdf",__dirname+"/public/WV Presentation Q1 2013.png",function(err,fp){
         console.log(err);
         console.log(fp);
         cb(err,{"path":fp});
